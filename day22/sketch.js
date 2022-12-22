4662e2081fbdc8fe17afe51a04303f02391f6611
ushisha <usisha.s@gmail.com>
2022-12-22 17:23:20 +0000

    Create irregular sandy lines graphic

--- day22/sketch.js
+++ day22/sketch.js
@@ -1,7 +1,17 @@
+let totalPts = 600
+let steps = totalPts + 1
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(720, 720)
+  stroke(78, 251, 4)
+  frameRate(5)
 }
 
 function draw() {
-  background(220);
+  background(0)
+  let rand = 0
+  for (let i = 1; i < steps; i++) {
+    point((width / steps) * i, height / 2 + random(-rand, rand))
+    rand += random(-5, 5)
+  }
 }
