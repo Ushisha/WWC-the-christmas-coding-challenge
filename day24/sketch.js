2644153fb28217ae309cfd99150a4f24ee7afb07
ushisha <usisha.s@gmail.com>
2022-12-24 16:39:01 +0000

    Create textured globe

--- day24/sketch.js
+++ day24/sketch.js
@@ -1,7 +1,26 @@
+let img
+let theta = 0
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(720, 720, WEBGL)
+  img = loadImage('./image.jpg')
+  vid = createVideo(['assets/360video_256crop_v2.mp4'])
+  vid.elt.muted = true
+  vid.loop()
+  vid.hide()
 }
 
 function draw() {
-  background(220);
+  background(250)
+  translate(-100, 0, 0)
+  push()
+  rotateZ(theta * mouseX * 0.001)
+  rotateX(theta * mouseX * 0.001)
+  rotateY(theta * mouseX * 0.001)
+  //pass image as texture
+  texture(img)
+  sphere(150)
+  pop()
+
+  theta += 0.05
 }
